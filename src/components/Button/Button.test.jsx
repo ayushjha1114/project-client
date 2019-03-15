53ec609aee1a22f451de23d5f5cd749d787d17f6
ayushj117 <ayush.jha@successive.tech>
2019-03-15 18:43:43 +0530

    test: 💍 add test cases in some components

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Button from './Button';
 
 describe('Button', () => {
-  it('should render correctly', () => {
+  test('should render correctly', () => {
     const component = shallow(<Button />);
     expect(component).toMatchSnapshot();
+  });
+  it('check the disabled props', () => {
+    const props = {
+      disabled: true,
+    };
+
+    const component = mount(<Button {...props} />);
+    expect(component.prop('disabled')).toEqual(true);
   });
 });
